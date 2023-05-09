717c35db1f83dac7acf780065d9ea5dddf53f757
kk945954 <kk740924@msn.com>
2023-05-09 16:19:38 -0400

    env vars

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
-import { API_URL } from './src/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': API_URL
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': process.env.VITE_API_URL
+      }
     }
-  }
-})
+  })
+}
