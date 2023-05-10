10a466d7d66b74a73e887c551bc4d67c5567b539
kk945954 <kk740924@msn.com>
2023-05-10 00:21:19 -0400

    login fix v_2

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ const db = require('./db');
 const Ramen = require('./models/ramenModel');
 const User = require('./models/userModel');
 const ramenRouter = require('./routes/ramensRoute');
+const userRouter = require('./routes/usersRoute');
+
 const cors = require('cors');
 
 const app = express();
@@ -14,41 +16,42 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/ramens", ramenRouter);
+app.use("/api/users", userRouter);
   
 
 
-app.post("/api/users/register", (req, res) => {
-    const {name, email, password} = req.body;
-    const newUser = new User({name, email, password});
-    try {
-        newUser.save();
-        res.send("User registered");
-    } catch (err) {
-        return res.status(400).json({error: err});
-    }
+// app.post("/api/users/register", (req, res) => {
+//     const {name, email, password} = req.body;
+//     const newUser = new User({name, email, password});
+//     try {
+//         newUser.save();
+//         res.send("User registered");
+//     } catch (err) {
+//         return res.status(400).json({error: err});
+//     }
     
-});
+// });
 
-app.post("/api/users/login",async (req, res) => {
-    const {email, password} = req.body;
-    try {
-        const user = await User.find({email, password});
-        if (user.length > 0) {
-            const currentUser = {
-                name: user[0].name,
-                email: user[0].email,
-                isAdmin: user[0].isAdmin,
-                _id: user[0]._id
-            }
-            res.send(currentUser);
-        } else {
-            return res.status(400).json({message: "User login failed"});
-        }
-    }
-    catch (error) {
-        return res.status(400).json({error});     
-    }
-});
+// app.post("/api/users/login",async (req, res) => {
+//     const {email, password} = req.body;
+//     try {
+//         const user = await User.find({email, password});
+//         if (user.length > 0) {
+//             const currentUser = {
+//                 name: user[0].name,
+//                 email: user[0].email,
+//                 isAdmin: user[0].isAdmin,
+//                 _id: user[0]._id
+//             }
+//             res.send(currentUser);
+//         } else {
+//             return res.status(400).json({message: "User login failed"});
+//         }
+//     }
+//     catch (error) {
+//         return res.status(400).json({error});     
+//     }
+// });
 
 const port = process.env.PORT;
 
