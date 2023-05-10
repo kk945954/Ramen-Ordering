e2a1b0cafda9ca8ae49533eb1107d28a7bb47f44
kk945954 <kk740924@msn.com>
2023-05-09 20:11:37 -0400

    api connection fixed_3

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,26 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react';
 
-export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+// export default ({ mode }) => {
+//   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
 
-  return defineConfig({
-    plugins: [react()],
-    server: {
-      proxy: {
-        '/api': process.env.VITE_API_URL
-      }
+//   return defineConfig({
+//     plugins: [react()],
+//     server: {
+//       proxy: {
+//         '/api': process.env.VITE_API_URL
+//       }
+//     }
+//   })
+// }
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'https://ramen-ordering.onrender.com'
     }
-  })
-}
+  }
+})
+
+
